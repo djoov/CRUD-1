f6e7542597a52b53c6cf2ec93e285e6e1f5d6192
The djooveazt <63140876+djoov@users.noreply.github.com>
2025-06-20 15:02:15 +0700

    Add files via upload

---
+++ aset_202506201501.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.aset (nama_asset,jenis_asset,deskripsi_asset,tahun_pengadaan,kode_asset) VALUES
+	 ('Komputer Dell OptiPlex','Elektronik','Komputer untuk staff administrasi','2023','PC-001'),
+	 ('Meja Kerja Olympic','Furnitur','Meja kerja kayu dengan 3 laci','2022','MJ-001'),
+	 ('Kursi Direktur Chairman','Furnitur','Kursi hidrolik kulit warna hitam','2022','CRS-001'),
+	 ('Proyektor Epson EB-X500','Elektronik','Proyektor untuk ruang meeting utama','2024','PRJ-001'),
+	 ('AC Daikin 1 PK','Elektronik','Pendingin ruangan untuk lobi','2023','AC-001');
